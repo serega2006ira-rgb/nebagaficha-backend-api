2039971d4e76e4001c8cdae9bf404ac8bbc11b68
Сергей Данилкин <sergejdanilkin@Mac-mini-Sergej.local>
2025-11-04 22:41:11 +0300

    feat: initial backend setup (cleaned secret)

---
+++ netlify/functions/github-auth.js
@@ -0,0 +1,22 @@
+// github-auth.js
+
+const jwt = require('jsonwebtoken'); // <-- ЭТО КРИТИЧНО! ДОБАВИТЬ!
+
+exports.handler = async (event, context) => {
+    // Получаем CLIENT_ID из переменных окружения Netlify
+    const CLIENT_ID = process.env.GITHUB_CLIENT_ID; 
+
+    // Формируем URL для возврата 
+    const REDIRECT_URI = 'https://fantastic-piroshki-8a73bc.netlify.app/.netlify/functions/github-callback';
+
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user:email`;
+
+    // Перенаправление пользователя на GitHub
+    return {
+        statusCode: 302,
+        headers: {
+            'Location': githubAuthUrl,
+        },
+        body: ''
+    };
+};
